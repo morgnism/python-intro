a4136ec491aec1f1bd7da4eff58cc2bc3eaefc0e
Morgan Worrell <hello@morganism.codes>
2023-11-25 19:35:24 -0700

    alternate shorter loop prompt

--- number.py
+++ number.py
@@ -1,9 +1,8 @@
 while True:
     try:
         x = int(input("What's x? "))
+        break
     except ValueError:
         print("x is not an integer")
-    else:
-        break
 
 print(f"x is {x}")
