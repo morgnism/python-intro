6a27f05369341b8dc64be48004d0c4866e5e0381
Morgan Worrell <hello@morganism.codes>
2023-11-25 18:12:43 -0700

    using funciton method conditionals

--- parity.py
+++ parity.py
@@ -1,6 +1,12 @@
-x = int(input("What's x? "))
+def main():
+    x = int(input("What's x? "))
 
-if x % 2 == 0:
-    print("even")
-else:
-    print("odd")
+    if is_even(x):
+        print("even")
+    else:
+        print("odd")
+
+def is_even(n):
+    return n % 2 == 0
+
+main()
