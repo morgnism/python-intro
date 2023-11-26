cde9b8fddbb0fbe6ee2e8b861df3191df12c4ce1
Morgan Worrell <hello@morganism.codes>
2023-11-25 19:08:29 -0700

    nested loop abstractions

--- mario.py
+++ mario.py
@@ -2,13 +2,10 @@ def main():
     print_square(3)
     
 def print_square(size):
-    # for each row of bricks
     for i in range(size):
-            
-        # for each brick in a row
-        for j in range(size):
-            print("#", end="")
-            
-        print()
+        print_row(size)
+
+def print_row(width):
+    print("#" * width)
 
 main()
