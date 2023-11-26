538d851abc2baf29404a4949df1f96e37c5cf3ec
Morgan Worrell <hello@morganism.codes>
2023-11-25 19:38:24 -0700

    continually prompt correct response in functions

--- number.py
+++ number.py
@@ -1,8 +1,12 @@
-while True:
-    try:
-        x = int(input("What's x? "))
-        break
-    except ValueError:
-        print("x is not an integer")
+def main():
+    x = get_int();
+    print(f"x is {x}")
 
-print(f"x is {x}")
+def get_int():
+    while True:
+        try:
+            return int(input("What's x? "))
+        except ValueError:
+            print("x is not an integer")
+
+main()
