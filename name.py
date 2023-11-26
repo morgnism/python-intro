f95e86bd3d8b1042c7a283e43dac57f5eb571b1d
Morgan Worrell <hello@morganism.codes>
2023-11-26 14:17:56 -0700

    handling exceptions with conditionals

--- name.py
+++ name.py
@@ -3,7 +3,9 @@ import sys
 # sys.argv[0] is the name of the program running
 # print(sys.argv[0])
 
-try:
-    print("hello, my name is", sys.argv[1])
-except IndexError:
+if len(sys.argv) < 2:
     print("Too few arguments")
+elif len(sys.argv) > 2:
+    print("Too many arguments")
+else:
+    print("hello, my name is", sys.argv[1])
