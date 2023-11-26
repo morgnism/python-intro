e4823476579acfd08e33f20bb902b89dc226030c
Morgan Worrell <hello@morganism.codes>
2023-11-25 19:08:01 -0700

    nested loops

--- mario.py
+++ mario.py
@@ -1,7 +1,14 @@
 def main():
-    print_row(4)
+    print_square(3)
     
-def print_row(width):
-    print("?" * width)
+def print_square(size):
+    # for each row of bricks
+    for i in range(size):
+            
+        # for each brick in a row
+        for j in range(size):
+            print("#", end="")
+            
+        print()
 
 main()
