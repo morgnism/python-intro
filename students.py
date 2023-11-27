fbe313817906e4311fe5d80eac5a5389d523f9c0
Morgan Worrell <hello@morganism.codes>
2023-11-26 20:29:10 -0700

    sorting list of dicts using 'key' comparitor function

--- students.py
+++ students.py
@@ -6,5 +6,11 @@ with open("students.csv") as file:
         student = { "name": name, "house": house }
         students.append(student)
 
-for student in students:
+def get_name(student):
+    return student["name"]
+
+def get_house(student):
+    return student["house"]
+
+for student in sorted(students, key=get_house):
     print(f"{student['name']} is in {student['house']}")
