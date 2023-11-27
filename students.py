580afcb1dfbe2d79812f016cced60dd0467172dd
Morgan Worrell <hello@morganism.codes>
2023-11-26 20:40:34 -0700

    using csv reader to parse complex csv data

--- students.py
+++ students.py
@@ -1,10 +1,11 @@
+import csv
+
 students = []
 
 with open("students.csv") as file:
-    for line in file:
-        name, house = line.rstrip().split(",")
-        student = { "name": name, "house": house }
-        students.append(student)
+    reader = csv.reader(file)
+    for name, home in reader:
+        students.append({ "name": name, "home": home })
 
 for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is in {student['house']}")
+    print(f"{student['name']} is in {student['home']}")
