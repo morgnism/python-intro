b2526c25bcdf69be614a8ebb69baad2e03f6eae4
Morgan Worrell <hello@morganism.codes>
2023-11-26 20:49:18 -0700

    writing to a csv file

--- students.py
+++ students.py
@@ -1,11 +1,8 @@
 import csv
 
-students = []
+name = input("What's your name? ")
+home = input("Where's your home? ")
 
-with open("students.csv") as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        students.append({ "name": row["name"], "home": row["home"] })
-
-for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is in {student['home']}")
+with open("students.csv", "a") as file:
+    writer = csv.writer(file)
+    writer.writerow([name, home])
