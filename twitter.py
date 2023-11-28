0811e234806987ce3255574051412513b565ef9b
Morgan Worrell <hello@morganism.codes>
2023-11-27 18:55:57 -0700

    ignoring capture groups in extraction a string

--- twitter.py
+++ twitter.py
@@ -2,5 +2,5 @@ import re
 
 url = input("URL: ").strip()
 
-username = re.sub(r"^(https?://)?(www\.)?twitter\.com/", "")
-print(f"Username: {username}")
+if matches:= re.search(r"^https?://(?:www\.)?twitter\.com/([a-z0-9_]+)", url, re.IGNORECASE):
+    print(f"Username: ", matches.group(1))
