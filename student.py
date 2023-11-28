94155200ce30c9c779151ccec8671cd27327122b
Morgan Worrell <hello@morganism.codes>
2023-11-27 19:25:34 -0700

    instantiating a new class object

--- student.py
+++ student.py
@@ -1,15 +1,19 @@
 class Student:
-    ...
+    # equal to the constructor in JavaScript
+    # self == this in JavaScript. 'self' is convention
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
     
 def main():
     student = get_student()
     print(f"{student.name} from {student.house}")
 
 def get_student():
-    student = Student()
-    student.name = input("Name: ")
-    student.house = input("House: ")
-    return student
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house)
 
 if __name__ == "__main__":
     main()
