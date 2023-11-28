eaa4b8ac61bd0bbcfe492832cf0d78737c86fa2e
Morgan Worrell <hello@morganism.codes>
2023-11-27 18:50:11 -0700

    replacing with regex substitution

--- twitter.py
+++ twitter.py
@@ -1,4 +1,6 @@
+import re
+
 url = input("URL: ").strip()
 
-username = url.removeprefix("https://twitter.com/", "")
+username = re.sub(r"^(https?://)?(www\.)?twitter\.com/", "")
 print(f"Username: {username}")
