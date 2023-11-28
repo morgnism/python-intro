510d6924535041ba3613167c7f20a1ee36cf375a
Morgan Worrell <hello@morganism.codes>
2023-11-27 18:34:26 -0700

    simplified regex match with groups

--- format.py
+++ format.py
@@ -2,10 +2,9 @@ import re
 
 name = input("What's your name? ").strip()
 
-matches = re.search(r"^(.+), (.+)$", name)
+matches = re.search(r"^(.+), ?(.+)$", name)
 
 if matches:
-    last, first = matches.groups()
-    name = f"{first} {last}"
+    name = matches.group(2) + " " + matches.group(1)
 
 print(f"hello, {name}")
