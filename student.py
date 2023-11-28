64db3d46f0aabf570ff34d48fcbb520d021337f3
Morgan Worrell <hello@morganism.codes>
2023-11-27 19:07:14 -0700

    converting to mutable list to override values

--- student.py
+++ student.py
@@ -1,11 +1,13 @@
 def main():
     student = get_student()
+    if student[0] == "Padma":
+        student[1] = "Ravenclaw"
     print(f"{student[0]} from {student[2]}")
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return (name, house)
+    return [name, house] # <--- change tuple to list to convert between immutable to mutable
 
 if __name__ == "__main__":
     main()
