86b3f8d87ef7ed6c3fa4a20d06cbeff7b37dabf6
Morgan Worrell <hello@morganism.codes>
2023-11-30 21:10:28 -0700

    init setup

---
+++ houses.py
@@ -0,0 +1,7 @@
+students = [
+    {"name": "Hermoine", "house": "Gryffindor"},
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"},
+    {"name": "Padma", "house": "Ravenclaw"},
+]
