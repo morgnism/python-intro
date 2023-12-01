33779300cd8c563f2dba0fbab0cd3b4e45ea706a
Morgan Worrell <hello@morganism.codes>
2023-11-30 20:33:51 -0700

    instantiate a class object

--- hat.py
+++ hat.py
@@ -1,4 +1,12 @@
+import random
+
+# use classes when you want to represent real-world entities
 class Hat:
-    ...
+    def __init__(self):
+        self.houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+    
+    def sort(self, name):
+        print(name, "is in", random.choice(self.houses))
 
 hat = Hat()
+hat.sort("Harry")
