8f17f2de13c5f976532994a4a8d3ea715265dec6
Morgan Worrell <hello@morganism.codes>
2023-11-30 21:14:27 -0700

    using 'set' to filter

--- houses.py
+++ houses.py
@@ -5,3 +5,11 @@ students = [
     {"name": "Draco", "house": "Slytherin"},
     {"name": "Padma", "house": "Ravenclaw"},
 ]
+
+houses = set() # only stores one of each element in a list
+
+for student in students:
+    houses.add(student["house"]) # set automatically filters dupes
+
+for house in sorted(houses):
+    print(house)
