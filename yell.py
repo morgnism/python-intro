ecd02682825ea5b6988743bb80230a5f63a0d95b
Morgan Worrell <hello@morganism.codes>
2023-12-01 22:01:59 -0700

    initial setup

---
+++ yell.py
@@ -0,0 +1,11 @@
+def main():
+    yell(["This", "is", "SPARTA"])
+
+def yell(words):
+    uppercased = []
+    for word in words:
+        uppercased.append(word.upper())
+    print(*uppercased)
+
+if __name__ == "__main__":
+    main()
