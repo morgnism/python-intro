1b8290194c1a3646172ae25857087542780e229e
Morgan Worrell <hello@morganism.codes>
2023-12-01 21:46:06 -0700

    unpacking args into a function via dict of same length

--- unpack.py
+++ unpack.py
@@ -3,7 +3,7 @@
 def total(galleons, sickles, knuts):
     return (galleons * 17 + sickles) * 29 + knuts;
 
-const = [100, 50, 25];
+coins = {"galleons": 100, "sickles": 50, "knuts": 25};
 
 # similar to spreading args in JavaScript
-print(total(*const), "Knuts")
+print(total(**coins), "Knuts")
