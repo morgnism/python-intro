b7a7b4776e1dcffc8f2111d6848d6328dca49237
Morgan Worrell <hello@morganism.codes>
2023-12-01 22:29:27 -0700

    simplifying main by lifting more functionality to another function

--- sleep.py
+++ sleep.py
@@ -1,10 +1,13 @@
 def main():
     n = int(input("What's n? " ))
-    for i in range(n):
-        print(sheep(i))
+    for s in sheep(n):
+        print(s)
 
 def sheep(n):
-    return "🐑" * n
+    flock = []
+    for i in range(n):
+        flock.append("🐑" * i)
+    return flock
 
 if __name__ == "__main__":
     main()
