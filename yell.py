1c46c3aca0cc729f5a5bd152aa9577301c35b116
Morgan Worrell <hello@morganism.codes>
2023-12-01 22:04:10 -0700

    simplifying common functionality with maps

--- yell.py
+++ yell.py
@@ -2,9 +2,7 @@ def main():
     yell("This", "is", "SPARTA")
 
 def yell(*words):
-    uppercased = []
-    for word in words:
-        uppercased.append(word.upper())
+    uppercased = map(str.upper, words)
     print(*uppercased)
 
 if __name__ == "__main__":
