6f473acc9359b566e878c7ce7a59c123572be935
Morgan Worrell <hello@morganism.codes>
2023-12-01 22:19:24 -0700

    simplifying with with list comprehension

--- griffindors.py
+++ griffindors.py
@@ -1,8 +1,5 @@
 students = ["Hermoine", "Harry", "Ron"]
 
-gryffindors = []
-
-for student in students:
-    gryffindors.append({"name": student, "house": "Gryffindor"})
+gryffindors = [{"name": student, "house": "Gryffindor"} for student in students]
 
 print(gryffindors)
