58babf177b3b1162e0fb9759bba8a3af7038da3f
Morgan Worrell <hello@morganism.codes>
2023-12-01 21:55:54 -0700

    spreading positional kwargs param in functions

--- unpack.py
+++ unpack.py
@@ -7,6 +7,6 @@
 #           maintains the same order passed in.
 #           converts automatically to a dictionary
 def f(*args, **kwargs):
-    print("Positional:", args)
+    print("Named:", kwargs)
 
-f(100, 50, 25)
+f(galleons=100, sickles=50, knuts=25)
