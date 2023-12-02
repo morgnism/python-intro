47e3aac0b54e9e24e3292c9c6c98a4c212f2cbbd
Morgan Worrell <hello@morganism.codes>
2023-12-01 21:55:09 -0700

    spreading positional args param in functions

--- unpack.py
+++ unpack.py
@@ -1,9 +1,12 @@
-# spread args into the function an stay verbose instead of
-# defining ...args like in JavaScript
-def total(galleons, sickles, knuts):
-    return (galleons * 17 + sickles) * 29 + knuts;
+# spread args into the function closer to ...args like in JavaScript
+# in python:
+#   args = positional arguments.
+#          maintains the same order passed in.
+#          converts to a tuple if only one list element
+#   kwargs = named arguments.
+#           maintains the same order passed in.
+#           converts automatically to a dictionary
+def f(*args, **kwargs):
+    print("Positional:", args)
 
-coins = {"galleons": 100, "sickles": 50, "knuts": 25};
-
-# similar to spreading args in JavaScript
-print(total(**coins), "Knuts")
+f(100, 50, 25)
